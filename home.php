6eab49b60a2f3fa2b46e514b258e0e88d905f891
Kevin Araujo <kevinaraujoo677@gmail.com>
2021-03-22 01:38:14 -0300

    verficando se o usuario tem itinerario

--- home.php
+++ home.php
@@ -5,10 +5,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Itinerários</title>
 </head>
 
 <body>
+    <?php
+    require_once "includes/banco.php";
+    require_once "includes/funcoes.php";
+    require_once "includes/login.php";
+    require_once "includes/head.php";
+
+    ?>
+
+    <div>
+        <?php
+        $busca = $banco->query("select nick from usu_it");
+        while ($reg = $busca->fetch_object()) {
+            if ($reg->nick === $_SESSION['user']) {
+                $verify = true;
+            } else {
+                $verify = false;
+            }
+        }
+
+        if ($verify === true) {
+            echo "tem";
+        } else {
+            echo "Parece que é uma das primeiras vezes que você entra aqui, <a href='itinerarios.php'>escolha seus itinerarios</a>";
+        }
+        ?>
+        </table>
+    </div>
+
 
 </body>
 
